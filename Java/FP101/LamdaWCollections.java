589ad113322186f1323cc5320f627ce86d5f63a4
mazeed <janujani819@gmail.com>
2025-08-30 00:46:12 +0530

    Add files via upload

---
+++ Java/FP101/LamdaWCollections.java
@@ -0,0 +1,14 @@
+package AdvancedJava.FP;
+import java.util.*;
+public class LamdaWCollections {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("ace","alice","bob","john");
+        /* traditional way would be to use loop */
+        for (String s : names){
+            System.out.println(s);
+        }
+        /* functional way to do so*/
+        names.forEach(name-> System.out.println(name)); /* using lambda */
+        names.forEach(System.out::println); /* method reference */
+    }
+}
